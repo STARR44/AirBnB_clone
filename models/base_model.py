b87ab0aec04208926ad11bd31c5e71dc12f8d6bb
STARR44 <okekeesther100@gmail.com>
2024-05-17 13:22:10 -0700

    updated attributes

--- models/base_model.py
+++ models/base_model.py
@@ -6,12 +6,20 @@ import datetime
 class BaseModel:
     """This class define all common attributes/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes instance of class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
 
+        if kwargs:
+            for key, value in items():
+                if key in "__class__":
+                    continue
+                if key in ("created_at", "updated_at"):
+                    value = datetime.datetime.fromisoformat(value)
+                    setattr(self, key, value)
+                else:
+                    self.id = str(uuid.uuid4())
+                    self.created_at = datetime.datetime.now()
+                    self.updated_at = datetime.datetime.now()
     def __str__(self):
         """return string representation of t object"""
         return f"[BaseModel]({self.id}) {self.__dict__}"
@@ -20,7 +28,7 @@ class BaseModel:
         """updates <updated_at> with the current datetime"""
         updated_at = datetime.datetime.now()
 
-    def to_dict(self):
+   def to_dict(self):
         """returns a dictionary containing\
             all keys/values of __dict__ of the instance"""
         self.__dict__['__class__'] = 'BaseModel'
